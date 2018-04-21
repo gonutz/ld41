06842fefab4b6bd2cbb7394562d19795d6bd525d
Lars <lars.scheme@gmail.com>
2018-04-21 09:30:54 +0200

    Add hero image

--- playing.go
+++ playing.go
@@ -2,15 +2,44 @@ package main
 
 import "github.com/gonutz/prototype/draw"
 
-type playingState struct{}
+const (
+	playerSpeed      = 4
+	playerW, playerH = 93, 209
+)
 
-func (*playingState) enter(state) {}
-func (*playingState) leave()      {}
+type playingState struct {
+	playerX          int
+	playerFacingLeft bool
+}
 
-func (*playingState) update(window draw.Window) state {
+func (s *playingState) enter(state) {
+	s.playerX = windowW / 4
+}
+
+func (*playingState) leave() {}
+
+func (s *playingState) update(window draw.Window) state {
+	// handle input
 	if window.WasKeyPressed(draw.KeyEscape) {
+		// TODO eventually go to a pause menu here
 		window.Close()
 	}
-	window.DrawScaledText("TODO: game", 10, 10, 3, draw.White)
+	if window.IsKeyDown(draw.KeyLeft) {
+		s.playerX -= playerSpeed
+		s.playerFacingLeft = true
+	}
+	if window.IsKeyDown(draw.KeyRight) {
+		s.playerX += playerSpeed
+		s.playerFacingLeft = false
+	}
+
+	// render
+	window.FillRect(0, 0, windowW, windowH, draw.RGB(0, 0.5, 1))
+	if s.playerFacingLeft {
+		window.DrawImageFile("rsc/hero left.png", s.playerX, 250)
+	} else {
+		window.DrawImageFile("rsc/hero right.png", s.playerX, 250)
+	}
+
 	return playing
 }
