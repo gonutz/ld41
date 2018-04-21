0d57f5f3cb554bd4377ba65c1ce2939774d2ad7b
Lars <lars.scheme@gmail.com>
2018-04-21 08:42:50 +0200

    Use prototype library to show empty window

--- main.go
+++ main.go
@@ -1,7 +1,22 @@
 package main
 
-import "fmt"
+import "github.com/gonutz/prototype/draw"
+
+const (
+	title            = "LD41 - TODO: name this game"
+	windowW, windowH = 1000, 600
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	check(draw.RunWindow(title, windowW, windowH, func(window draw.Window) {
+		if window.WasKeyPressed(draw.KeyEscape) {
+			window.Close()
+		}
+	}))
+}
+
+func check(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
