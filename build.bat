eb729f30b3765b91439e0976c217599c10810642
Lars <lars.scheme@gmail.com>
2018-04-22 00:57:15 +0200

    Rename output exe in build script

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
 set GOARCH=386
 go build -ldflags "-H=windowsgui -s -w" -o game.exe
 blob -folder=rsc -out=rsc.blob
-payload -data=rsc.blob -exe=game.exe -output=Shootematics.exe
+payload -data=rsc.blob -exe=game.exe -output="No-Brain Jogging.exe"
 del game.exe
