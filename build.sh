32958add24aed21fd0463422a68cae04b1b3134c
gonutz <gonutz@fake.mail>
2025-07-06 22:35:17 +0200

    Update Linux build script to use embed package

--- build.sh
+++ build.sh
@@ -1,23 +1,11 @@
 #!/bin/bash
 
-# build the tools that are necessary to build the final game
-go build ./vendor/github.com/gonutz/blob/cmd/blob
-go build ./vendor/github.com/gonutz/payload/cmd/payload
-go build ./vendor/github.com/gonutz/rsrc
+# Get the necessary build tool.
+go install github.com/gonutz/rsrc@latest
 
-# build the resource file with the icon so the Go build tool adds it to the executable
+REM Build the resource files with the icon so the Go build tool adds it to the executable.
 ./rsrc -arch 386 -ico icon.ico -o rsrc_386.syso
 ./rsrc -arch amd64 -ico icon.ico -o rsrc_amd64.syso
 
-# build a 32 bit Linux executable that runs on both 32 and 64 bit Linux
-set GOARCH=386
-go build -ldflags "-s -w" -o game
-./blob -folder=rsc -out=rsc.blob
-./payload -data=rsc.blob -exe=game -output="No-Brain Jogging"
-
-# delete the temporary helper files
-rm rsc.blob
-rm game
-rm blob
-rm payload
-rm rsrc
+# Build the executable.
+go build -ldflags "-s -w" -o "No-Brain Jogging"
