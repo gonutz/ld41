e42daaf0180d9154eb53f00e567ce29a0793eb47
gonutz <gonutz@fake.mail>
2025-07-06 22:30:23 +0200

    Update Windows build script
    These days we use the embed package so our self-rolled blob and payload
    are not necessary anymore.

--- build.bat
+++ build.bat
@@ -1,21 +1,10 @@
-REM build the tools that are necessary to build the final game
-go build ./vendor/github.com/gonutz/blob/cmd/blob
-go build ./vendor/github.com/gonutz/payload/cmd/payload
-go build ./vendor/github.com/gonutz/rsrc
+REM Get the necessary build tool.
+go install github.com/gonutz/rsrc@latest
 
-REM build the resource files with the icon so the Go build tool adds it to the .exe
+REM Build the resource files with the icon so the Go build tool adds it to the executable.
 rsrc -arch 386 -ico icon.ico -o rsrc_386.syso
 rsrc -arch amd64 -ico icon.ico -o rsrc_amd64.syso
 
-REM build a 32 bit Windows exe that runs on all Windows from XP up
+REM Build a 32 bit Windows exe that runs on all Windows from XP up.
 set GOARCH=386
-go build -ldflags "-H=windowsgui -s -w" -o game.exe
-blob -folder=rsc -out=rsc.blob
-payload -data=rsc.blob -exe=game.exe -output="No-Brain Jogging.exe"
-
-REM delete the temporary helper files
-del rsc.blob
-del game.exe
-del blob.exe
-del payload.exe
-del rsrc.exe
+go build -ldflags "-H=windowsgui -s -w" -o "No-Brain Jogging.exe"
